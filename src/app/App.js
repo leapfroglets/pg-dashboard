e1e9b438b465f7d67beb4100376d3adc6a9cd7d5
aanchal <aanchal.adk@gmail.com>
2017-08-22 17:19:48 +0545

    corrected changes

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import SqlEditor from './components/SqlEditor';
-<<<<<<< HEAD
 import SidePanel from './components/DatabaseList';
-=======
-
->>>>>>> f8389074a368b5784249f97867a1548dae2e0081
 
 class App extends Component {
   
@@ -13,10 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <SqlEditor />
-<<<<<<< HEAD
         <SidePanel />
-=======
->>>>>>> f8389074a368b5784249f97867a1548dae2e0081
       </div>
     );
   }
