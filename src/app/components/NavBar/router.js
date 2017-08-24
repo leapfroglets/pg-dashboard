e5784f2749b930041977b44b835c944171206f22
aanchal <aanchal.adk@gmail.com>
2017-08-24 13:57:49 +0545

    more changes to browse

--- src/app/components/NavBar/router.js
+++ src/app/components/NavBar/router.js
@@ -5,57 +5,72 @@ import Browse from '../Browse';
 import Insert from '../Insert';
 import Operations from '../Operations';
 
-const NavRouter = (props) =>{
-  if(props.currTable!==null){
-    return(
-      <BrowserRouter>
-        <div>
-          <ul className = "nav nav-tabs bar_tabs">
-            <li className ="active"><Link to = "/browse">Browse</Link></li>
-            <li><Link to = "/sqlEditor">Sql Editor</Link></li>
-            <li><Link to = "/insert">Insert</Link></li>
-            <li><Link to = "/operations">Operations</Link></li>
-          </ul>
-          <Switch>
-            <Route exact path = "/" 
-              exact render={() => (<Browse dbname={props.currDbname} table={props.currTable}/>)}
-            />
-            <Route path = "/browse" 
-              exact render={() => (<Browse dbname={props.currDbname} table={props.currTable}/>)}
-            />
-            <Route path = "/sqlEditor"
-              exact render={() => (<SqlEditor dbname={props.currDbname} table={props.currTable}/>)}
-            />
-            <Route path = "/insert"
-              exact render={() => (<Insert dbname={props.currDbname} table={props.currTable}/>)}
-            />
-            <Route path = "/operations"
-              exact render={() => (<Operations dbname={props.currDbname} table={props.currTable}/>)}
-            />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
+class NavRouter extends Component{
+  constructor(){
+    super();
+    this.state={
+      isActive:"browse"
+    };
   }
-  else{
-    return(
-      <BrowserRouter>
-        <div>
-          <ul className = "nav nav-tabs bar_tabs">
-            <li className ="active"><Link to = "/sqlEditor">Sql Editor</Link></li>
-            <li><Link to = "/insert">Insert</Link></li>
-            <li><Link to = "/operations">Operations</Link></li>
-          </ul>
-          <Switch>
-            <Route path = "/sqlEditor" component = {SqlEditor}/>
-            <Route path = "/insert" component = {Insert}/>
-            <Route path = "/operations" component = {Operations}/>
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
-  } 
+
+  changeTab(tab){
+    this.setState({
+      isActive:tab
+    })
   }
+  render(){
+    if(this.props.currTable!==null){
+      return(
+        <BrowserRouter>
+          <div>
+            <ul className = "nav nav-tabs bar_tabs">
+              <li className ={(this.state.isActive==="browse") ? "active":""} onClick = {() => {this.changeTab("browse")}}><Link to = "/browse">Browse</Link></li>
+              <li className ={(this.state.isActive==="sqlEditor") ? "active":""}onClick = {() => {this.changeTab("sqlEditor")}}><Link to = "/sqlEditor">Sql Editor</Link></li>
+              <li className ={(this.state.isActive==="insert") ? "active":""}onClick = {() => {this.changeTab("insert")}}><Link to = "/insert">Insert</Link></li>
+              <li className ={(this.state.isActive==="operations") ? "active":""}onClick = {() => {this.changeTab("operations")}}><Link to = "/operations">Operations</Link></li>
+            </ul>
+           
+              <Route exact path = "/" 
+                exact render={() => (<Browse dbname={this.props.currDbname} table={this.props.currTable}/>)}
+              />
+              <Route path = "/browse" 
+                exact render={() => (<Browse dbname={this.props.currDbname} table={this.props.currTable}/>)}
+              />
+              <Route path = "/sqlEditor"
+                exact render={() => (<SqlEditor dbname={this.props.currDbname} table={this.props.currTable}/>)}
+              />
+              <Route path = "/insert"
+                exact render={() => (<Insert dbname={this.props.currDbname} table={this.props.currTable}/>)}
+              />
+              <Route path = "/operations"
+                exact render={() => (<Operations dbname={this.props.currDbname} table={this.props.currTable}/>)}
+              />
+           
+          </div>
+        </BrowserRouter>
+      );
+    }
+    else{
+      return(
+        <BrowserRouter>
+          <div>
+            <ul className = "nav nav-tabs bar_tabs">
+              <li className ={(this.state.isActive==="sqlEditor") ? "active":""}onClick = {() => {this.changeTab("sqlEditor")}}><Link to = "/sqlEditor">Sql Editor</Link></li>
+              <li className ={(this.state.isActive==="insert") ? "active":""}onClick = {() => {this.changeTab("insert")}}><Link to = "/insert">Insert</Link></li>
+              <li className ={(this.state.isActive==="operations") ? "active":""}onClick = {() => {this.changeTab("operations")}}><Link to = "/operations">Operations</Link></li>
+            </ul>
+
+              <Route path = "/sqlEditor" component = {SqlEditor}/>
+              <Route path = "/insert" component = {Insert}/>
+              <Route path = "/operations" component = {Operations}/>
+
+          </div>
+        </BrowserRouter>
+      );
+    }
+  }
+   
+}
 
 
 export default NavRouter;
