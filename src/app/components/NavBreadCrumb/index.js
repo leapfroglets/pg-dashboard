b433cdf5d4bf9856145257df664bbba3cc303b8c
dibesh <dmsshrestha@gmail.com>
2017-08-25 11:12:54 +0545

    Update to NavBreadCrumb component

--- src/app/components/NavBreadCrumb/index.js
+++ src/app/components/NavBreadCrumb/index.js
@@ -5,12 +5,11 @@ class NavBreadCrumb extends Component {
       <div>
         <ol className='breadcrumb'>
           <li>{window.location.href.split('/')[2]}</li>
-          {this.props.currDbname!=null && <li>{this.props.currDbname}</li>}
-          {this.props.currTable!=null && <li>{this.props.currTable}</li>}
+          {this.props.currDbname != null && <li>{this.props.currDbname}</li>}
+          {this.props.currTable != null && <li>{this.props.currTable}</li>}
         </ol>
       </div>
     )
-
   }
 }
 export default NavBreadCrumb;
