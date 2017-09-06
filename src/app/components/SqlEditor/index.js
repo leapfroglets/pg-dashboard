1b11f2351322c3bfb70c72ad1758e5ae169d8cc7
DibeshMSShrestha <dmsshrestha@gmail.com>
2017-09-06 10:24:48 +0545

    Add CodeMirror js library to the SqlEditor Component

--- src/app/components/SqlEditor/index.js
+++ src/app/components/SqlEditor/index.js
@@ -1,104 +1,83 @@
 import React, { Component } from 'react';
 import SqlResult from './SqlResult';
 import './style.css';
+import CodeMirror from 'react-codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/sql/sql';
 class SqlEditor extends Component {
   constructor() {
     super();
     this.state = {
-      brokenQueries: [],
-      
+      brokenQueries: []
     };
-    this.selectQuery='';
+    this.options = { lineNumbers: true, mode: 'text/x-pgsql' };
     this.executeQuery = this.executeQuery.bind(this);
-    this.keyLog = this.keyLog.bind(this);
-    this.showSelection = this.showSelection.bind(this);
   }
-  showSelection(e) {
-    let textComponent = document.getElementById('textInput');
-    let selectedText;
-    if (textComponent.selectionStart !== undefined) {
-      // Standards Compliant Version
-      let startPos = textComponent.selectionStart;
-      let endPos = textComponent.selectionEnd;
-      if (startPos === endPos) return;
-      console.log(
-        startPos,
-        endPos,
-        textComponent.value,
-        textComponent.value.substring(startPos, endPos)
-      );
-      selectedText = textComponent.value.substring(startPos, endPos);
-      this.selectQuery=selectedText;
+  componentDidMount() {
+    if (this.props.currDbname !== null) {
+      let map = { 'Ctrl-Enter': () => this.executeQuery() };
+      this.refs.editor.getCodeMirror().addKeyMap(map);
     }
   }
-  keyLog(e) {
-    if (e.ctrlKey && e.keyCode === 13){
-      this.executeQuery(document.getElementById('textInput').value);
-      alert(1)
-    }
-    if (e.altKey && e.keyCode === 13){
-      this.executeQuery(this.selectQuery);
-      this.selectQuery='';
-      alert(2)
-    }
-      
-  }
-  executeQuery(query) {
+  executeQuery() {
+    let query = '';
+    if (this.refs.editor.getCodeMirror().getSelection() === '')
+      query = this.refs.editor.getCodeMirror().getValue();
+    else query = this.refs.editor.getCodeMirror().getSelection();
+
     if (query !== '') {
-      let queries = query.split(';').filter(oneQuery => {
-        if (oneQuery !== '\n') return oneQuery;
-        return null;
-      });
+      let queries = query
+        .trim()
+        .split(';')
+        .filter(oneQuery => {
+          if (oneQuery !== '\n') return oneQuery;
+          return null;
+        });
       this.setState({ brokenQueries: queries });
     }
   }
   render() {
     return (
-      <div>
-        <div className="x_panel">
-          <div>
-            <textarea
-              placeholder="Enter your SQL here"
-              className="form-control"
-              id="textInput"
-              onKeyDown={e => this.keyLog(e)}
-              onSelect={e => this.showSelection(e)}
-            />
-            <div>(Press [Ctrl]+Enter to Execute All)</div>
-            <div>(Press [Alt]+Enter to Execute Selected)</div>
+      this.props.currDbname !== null && (
+        <div>
+          <div className="x_panel">
+            <div>
+              <CodeMirror value="" options={this.options} ref="editor" />
+              <div>(Press [Ctrl]+Enter to Execute )</div>
+            </div>
+            <div className="ln_solid" />
+            <div>
+              <input
+                className="btn btn-round btn-default"
+                type="button"
+                value="execute"
+                onClick={() => {
+                  this.executeQuery();
+                }}
+              />
+            </div>
           </div>
-          <div className="ln_solid" />
-          <div>
-            <input
-              className="btn btn-round btn-default"
-              type="button"
-              value="execute"
-              onClick={() => {
-                this.executeQuery(document.getElementById('textInput').value);
-              }}
-            />
-          </div>
-        </div>
-        <div className="" id="output">
-          <div className="x_panel no-border">
-            {this.state.brokenQueries.map((q, i) => {
-              return (
-                <div key={i}>
-                  <div>
-                    <h2>Result of {q}</h2>
+          <div className="" id="output">
+            <div className="x_panel no-border">
+              {this.state.brokenQueries.map((q, i) => {
+                return (
+                  <div key={i}>
+                    <div>
+                      <h2>Result of {q}</h2>
+                    </div>
+                    <SqlResult
+                      currDbname={this.props.currDbname}
+                      query={q.trim()}
+                      key={i + 'q2'}
+                      refresh={() => this.props.refresh()}
+                    />
                   </div>
-                  <SqlResult
-                    currDbname={this.props.currDbname}
-                    query={q.trim()}
-                    key={i + 'q2'}
-                    refresh={() => this.props.refresh()}
-                  />
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
         </div>
-      </div>
+      )
     );
   }
 }
