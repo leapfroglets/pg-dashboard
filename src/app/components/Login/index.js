aad642f78f91628059a67e0b8962d9c7a6ac1a0c
DibeshMSShrestha <dmsshrestha@gmail.com>
2017-09-08 15:00:31 +0545

    PG-45 : Redesign UI of login page

--- src/app/components/Login/index.js
+++ src/app/components/Login/index.js
@@ -1,68 +1,73 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import * as httpUtil from '../../httpUtil';
 
-class Login extends Component{
-  constructor(){
+class Login extends Component {
+  constructor() {
     super();
-    this.state={
-      loggedIn:false
-    }
+    this.state = {
+      loggedIn: false,
+      error: ''
+    };
   }
-  logIn(e){
+  logIn(e) {
     e.preventDefault();
-    let userName=document.getElementById('userName').value;
-    let password=document.getElementById('password').value;
-    let hostName=document.getElementById('host').value;
-    let portNo=document.getElementById('port').value;
-    if(!userName || !password){
-      alert('username and Password fields cannot be left empty');
+    let userName = document.getElementById('userName').value;
+    let password = document.getElementById('password').value;
+    let hostName = document.getElementById('host').value;
+    let portNo = document.getElementById('port').value;
+    if (!userName || !password || !hostName || !portNo) {
+      this.setState({ error: 'Fields are empty' });
+    } else {
+      let data = {
+        user: userName,
+        password: password,
+        host: hostName,
+        port: portNo
+      };
+
+      httpUtil
+        .post('http://localhost:4553/api/database/login', data)
+        .then(res => {
+          this.props.changeState();
+          console.log(this.props.history);
+          this.props.history.push('/dashboard');
+        })
+        .catch(err => {
+          this.setState({ error: err.response.data.error.message });
+        });
     }
-    else{
-      let data={
-        user:userName,
-        password:password,
-        host:hostName,
-        port:portNo
-      }
-      
-      httpUtil.post('http://localhost:4553/api/database/login',data)
-      .then(res => {        
-        this.props.changeState();      console.log(this.props.history);
-        this.props.history.push('/dashboard');
-      })
-      .catch(err => {console.log(err)
-        let error = err.response.data.error.message;
-        alert(error)
-      });
-    }
-    
   }
 
-  render(){
-    return(
-      <form onSubmit={(e) => {
-            this.logIn(e);
-      }          
-      }>   
-        <div className='login-form-header'>
-          <img src='./images/logo2.png'/>
-          <h1>Welcome to PG-Admin</h1>
-        </div>
-        <div className='login-form'>
-          <div className='header'>
-            Sign In
+  render() {
+    return (
+      <div className="login">
+        <div className="img-div">
+          <img src="/images/ele.jpg" className="image" />
+          <div className="login-div">
+            <form
+              onSubmit={e => {
+                this.logIn(e);
+              }}
+            >
+              <div>
+                <h2>Welcome</h2>
+              </div>
+              <div>{this.state.error}</div>
+              <input type="text" id="userName" placeholder="User" />
+              <input type="password" id="password" placeholder="Password" />
+              <input type="host" id="host" placeholder="Host" />
+              <input type="port" id="port" placeholder="Port" />
+              <input
+                type="submit"
+                id="login-btn"
+                className="btn btn-round btn-default"
+                value="Log In"
+              />
+            </form>
           </div>
-          <input type='text' id='userName' placeholder='User Name'/>
-          <input type='password' id='password' placeholder='Password'/>
-          <input type='port' id='port' placeholder='port no.:5432'/>
-          <input type='host' id='host' placeholder='host:127.0.0.1'/>
-          <input type='submit' value='Log In' id='login' />
-          
         </div>
-     </form>
-    )
-    
+      </div>
+    );
   }
 }
-
 export default Login;
